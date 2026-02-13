46e7175131968fc8dd9e8730e2f0eb2fe045852a
Jeremy Mills <jeremymills496@gmail.com>
2026-02-12 16:32:32 -0800

    added executor + hlt instruction when no tasks in queue

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
 use bootloader::{BootInfo, entry_point};
 use chronos::println;
-use chronos::task::{Task,simple_executor::SimpleExecutor};
+use chronos::task::{Task, keyboard, executor::Executor};
 use x86_64::VirtAddr;
 use core::panic::PanicInfo;
 
@@ -41,8 +41,10 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     allocator::init_heap(&mut mapper, &mut frame_allocator)
         .expect("heap initialization failed");
 
-    let mut executor = SimpleExecutor::new();
+
+    let mut executor = Executor::new();
     executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses())); // new
     executor.run();
 
 
