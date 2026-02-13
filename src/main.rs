8ccff9ffb0aaa31ec2ed223efe861f86f1231f5e
Jeremy Mills <jeremymills496@gmail.com>
2026-02-12 16:03:20 -0800

    added task and simple executor

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,20 @@ use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
 use bootloader::{BootInfo, entry_point};
 use chronos::println;
+use chronos::task::{Task,simple_executor::SimpleExecutor};
 use x86_64::VirtAddr;
 use core::panic::PanicInfo;
 
 entry_point!(kernel_main);
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("number: {}", number);
+}
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
@@ -31,23 +41,11 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     allocator::init_heap(&mut mapper, &mut frame_allocator)
         .expect("heap initialization failed");
 
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
+    let mut executor = SimpleExecutor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.run();
 
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
 
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
     // For testing
     #[cfg(test)]
     test_main();
